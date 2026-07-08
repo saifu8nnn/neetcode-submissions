3fd3f3abad762ac3e1dcbf43b03484d6eff2d7cc
Saif codes <saifu8n@gmail.com>
2026-07-08 22:27:18 +0530

    Add: anagram-groups - submission-12

---
+++ Data Structures & Algorithms/anagram-groups/submission-12.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map={}
+        for s in strs:
+            sorted_s="".join(sorted(s))
+            if sorted_s not in anagram_map:
+                anagram_map[sorted_s]=[]  
+            anagram_map[sorted_s].append(s)
+        res=list(anagram_map.values())
+        return res
